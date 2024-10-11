34779daba42ed85ef283b1a3be7dd1c7d3b83452
bilal2819 <93931532+bilal2819@users.noreply.github.com>
2024-10-11 15:53:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=TnAn3JYb#trPtDtkgxjpK_70kFtKJA0T6YNIN_rb4vaOZ89_Di_k",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
